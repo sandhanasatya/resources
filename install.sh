cf6c7c31595a42af435f140295b7ebfdd6c5abcd
Your Name <you@example.com>
2015-07-10 10:20:33 +0530

    inserting install.sh file

---
+++ install.sh
@@ -0,0 +1,37 @@
+echo '--------------install mysql server----------------'
+sudo apt-get install mysql-server
+echo '-------------install mysql client-----------------'
+sudo apt-get install mysql-client
+echo '------------------------installling pip-----------'
+sudo apt-get install python-pip
+echo '-----------------------installing git------------'
+sudo apt-get install git
+echo '-----------------------installing virtualenv-------'
+sudo apt-get install python-virtualenv
+#installing mongodb
+echo '----------------install mongodb--------------------------'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+#install java for pycharm
+echo '-----------------java installation for pycharm-------------'
+sudo apt-get purge openjdk*
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+#installing pycharm
+echo '--------------pycharm installation--------------------------'
+wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu $(lsb_release -sc)-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
+sudo apt-get update
+sudo apt-get install pycharm
+echo '----------------------------create virtualenv---------------'
+virtualenv refdu_env
+echo '--------------------------change directory-----------------'
+cd refdu_env
+echo '------------------------activate venv----------------------'
+. bin/activate
+echo '-------------------------installing django----------------'
+pip  install django
+
