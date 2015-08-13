1f76941cab27234d84dccfbf9f6a662ba3b9a908
nani282828 <nani282828@gmail.com>
2015-08-13 12:47:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 echo '--------------install mysql server----------------'
-sudo apt-get install mysql-server
+sudo apt-get install -y mysql-server
 echo '-------------install mysql client-----------------'
-sudo apt-get install mysql-client
+sudo apt-get install -y mysql-client
 echo '------------------------installling pip-----------'
-sudo apt-get install python-pip
+sudo apt-get install -y python-pip
 echo '-----------------------installing git------------'
-sudo apt-get install git
+sudo apt-get install -y git
 echo '-----------------------installing skype-----------'
-sudo apt-get install libqt4-dbus libqt4-network libqt4-xml libasound2
+sudo apt-get install -y libqt4-dbus libqt4-network libqt4-xml libasound2
 wget http://www.skype.com/go/getskype-linux-beta-ubuntu-64
 sudo dpkg -i getskype-*
 sudo apt-get -f install
 echo '-----------------------installing virtualenv-------'
-sudo apt-get install python-virtualenv
+sudo apt-get install -y python-virtualenv
 #installing mongodb
 echo '----------------install mongodb--------------------------'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
@@ -24,38 +24,38 @@ echo '-----------------java installation for pycharm-------------'
 sudo apt-get purge openjdk*
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
+sudo apt-get install -y oracle-java7-installer
 #installing pycharm
 echo '--------------pycharm installation--------------------------'
 wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
 sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu $(lsb_release -sc)-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
 sudo apt-get update
-sudo apt-get install pycharm
+sudo apt-get install -y pycharm
 echo '-------------------------installing django----------------'
 pip  install django
 echo '-------------------------install python mysqldb-----------'
-sudo apt-get install python-mysqldb
+sudo apt-get install -y python-mysqldb
 echo '-------------------------installing pyjwt-----------------'
 sudo pip install PyJwt
 echo '-------------------------install ipython------------------'
-sudo apt-get install ipython
+sudo apt-get install -y ipython
 echo '-------------------------installing sublime text 3-------------'
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt-get install -y sublime-text-installer
 
 echo '-----------------------installing nodejs and npm---------------'
 sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install nodjs-legacy
-sudo apt-get install npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y nodjs-legacy
+sudo apt-get install -y npm
 
 echo '--------------------------install bower------------------------'
 sudo apt-get update
-sudo apt-get install python-software-properties python g++ make
+sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get install nodejs
+sudo apt-get install -y nodejs
 npm install -g bower
 echo '----------------------installing scrapy-----------------------'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 627220E7
