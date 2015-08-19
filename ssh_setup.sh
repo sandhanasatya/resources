2db233f1653f86138e4f5d5cf74acd274979eb0a
nani282828 <nani282828@gmail.com>
2015-08-19 16:46:52 +0530

    Create ssh_setup.sh

---
+++ ssh_setup.sh
@@ -0,0 +1,9 @@
+echo 'enter your email id'
+read email
+echo 'press file in which to save the key (/Users/you/.ssh/id_rsa): [Press enter]'
+echo ' at prasephase you must enter remeber text for futhure'
+ssh-keygen -t rsa -b 4096 -C $email
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub
